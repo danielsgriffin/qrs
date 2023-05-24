867bb4b8e8fcd6f5a8b6fd5380d7640cc8f23322
danielsgriffin <danielsgriffin@gmail.com>
2023-05-24 16:07:23 -0700

    removed Neeva

--- search.js
+++ search.js
@@ -9,8 +9,7 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
     'https://www.ecosia.org/search?q=%s',
     'https://www.google.com/search?q=%s',
     'https://www.qwant.com/?q=%s',
-    'https://you.com/search?q=%s',
-    'https://neeva.com/search?q=%s'];
+    'https://you.com/search?q=%s'];
   var listLen = presetList.length;
   var randomIdx = Math.floor(Math.random() * listLen);
   var newURL = presetList[randomIdx].replace("%s", encodeURIComponent(text));
